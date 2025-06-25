291fc059167de5ed22f58988e4b6efa16f379fe8
Ahmed Rizk <rezk18649@gmail.com>
2025-06-25 16:09:50 +0300

    Add edit view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/constants.dart';
 import 'package:notes_app/views/notes_view.dart';
 
 void main() {
--- lib/views/widgets/custom_notes_item.dart
+++ lib/views/widgets/custom_notes_item.dart
@@ -1,51 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class CustomNoteItem extends StatelessWidget {
   const CustomNoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 24, bottom: 24, left: 24),
-      decoration: BoxDecoration(
-        color: Color(0xffffcc80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            contentPadding: EdgeInsets.only(left: 0, right: 16),
-            title: Text(
-              "Flutter Tips",
-              style: TextStyle(color: Colors.black, fontSize: 24),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.only(top: 24, bottom: 24, left: 24),
+        decoration: BoxDecoration(
+          color: Color(0xffffcc80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              contentPadding: EdgeInsets.only(left: 0, right: 16),
+              title: Text(
+                "Flutter Tips",
+                style: TextStyle(color: Colors.black, fontSize: 24),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child: Text(
+                  "Build your career with tharwat smay",
+                  style: TextStyle(
+                    color: Colors.black.withValues(alpha: .5),
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                icon: Icon(Icons.delete_rounded, color: Colors.black, size: 30),
+                onPressed: () {},
+              ),
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16, bottom: 16),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
               child: Text(
-                "Build your career with tharwat smay",
+                "jun 24,2025",
                 style: TextStyle(
                   color: Colors.black.withValues(alpha: .5),
-                  fontSize: 18,
+                  fontSize: 20,
                 ),
               ),
             ),
-            trailing: IconButton(
-              icon: Icon(Icons.delete_rounded, color: Colors.black, size: 30),
-              onPressed: () {},
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              "jun 24,2025",
-              style: TextStyle(
-                color: Colors.black.withValues(alpha: .5),
-                fontSize: 20,
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
